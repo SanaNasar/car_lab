b1668634dd8c445e248d37f1921b797317985f7e
SanaNasar <sanasehereen.cse@gmail.com>
2014-07-17 09:15:03 -0700

    Adding js file for the homework

--- src/car.js
+++ src/car.js
@@ -1,5 +1,7 @@
 // require('locus');
 
+// Phase 1
+
 function Car(make, model, year, color){
   this.make = make;
   this.model = model;
@@ -19,20 +21,26 @@ Car.prototype.paint = function(newColor){
 	this.color = newColor;
 };
 
+
+//Car pickup and drop off
+
 Car.prototype.pickup = function(name) {
 	if (this.state==="on") {
 		console.log("Driving to pickup " +name);
 	}
+	else {
+		console.log("Car drop off " + name);
+	}
 	return;
 };
-};
+
+// Car park: Car#park only if the car is off and if it's off console.log parked!!
 
 Car.prototype.state = function(name) {
 	if (this.state==="off") {
 	console.log("The car is parked!");
-	return
+	return;
 	}
-};
 };
 
 var myCar = new Car('Audi', '201', '2008', 'blue');
